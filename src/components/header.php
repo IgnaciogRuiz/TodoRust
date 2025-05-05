7665a49374360ba1bd7285ec9e9e3bc481725899
Nacho ruiz <ignacioguillermoruiz@gmail.com>
2025-05-05 17:14:49 -0300

    estructura del proyecto

---
+++ src/components/header.php
@@ -0,0 +1,17 @@
+<?php
+echo '
+<nav>
+    <div class="logo">
+        <img src="./assets/images/cropped-rust-logo.png" alt="logo">
+    </div>
+    <div class="navbar">
+        <ul>
+            <li>Inicio</li>
+            <li>Guias</li>
+            <li>Bases</li>
+            <li>Sobre Nosotros</li>
+            <li>Mi Cuenta</li>
+        </ul>
+    </div>
+</nav>
+';
